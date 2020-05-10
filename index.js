c5b73428f971c254c991bfee0010591937b187e5
Lucas Maia <lucas.maia435@outlook.com>
2020-05-10 05:11:05 -0300

    Remove undefined error

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ const bot = new Twit(config);
 const post = () =>{
     axios.get("https://covid19-brazil-api.now.sh/api/report/v1/brazil")
     .then((response) => {
-        const deaths = response.data.data.deaths;
-        const cases = response.data.data.cases;
-        const recovered = response.data.data.recovered;
-        const total = response.data.data.confirmed;
-        let msg = `‼️ Casos confirmados (Totais) : ${total}\n`+
-        `😷 Portadores :  ${cases}\n` + 
-        `☑️ Curados :  ${recovered}\n`+ 
-        `✝️ Mortes : ${deaths}\n` +
-        `#covid19 #brasil #coronavirus #Covid19`;
-        
-        bot.post('statuses/update',{status:msg},(err)=>{
-            if(!err){
-                console.log("Houve alteração nos dados");
-            }
-        });
+        if(response.data.data.confirmed != undefined){
+            const deaths = response.data.data.deaths;
+            const cases = response.data.data.cases;
+            const recovered = response.data.data.recovered;
+            const total = response.data.data.confirmed;
+            let msg = `‼️ Casos confirmados (Totais) : ${total}\n`+
+            `😷 Portadores :  ${cases}\n` + 
+            `☑️ Curados :  ${recovered}\n`+ 
+            `✝️ Mortes : ${deaths}\n` +
+            `#covid19 #brasil #coronavirus #Covid19`;
+            
+            bot.post('statuses/update',{status:msg},(err)=>{
+                if(!err){
+                    console.log("Houve alteração nos dados");
+                }
+            });
+        }
     })
     .catch((err) => {
         console.log(err);
